15a91758c054c84bd810ffa7c5ad3119a6773e3a
FeerGonzalez <fernando.g.gonzalez@hotmail.com>
2023-04-21 13:22:55 -0300

    Comienzo del punto 3 tp4

---
+++ OO2 - TP4/src/ar/unrn/tp4/eje3/Main.java
@@ -0,0 +1,23 @@
+package ar.unrn.tp4.eje3;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					new Main().start();
+				} catch (Exception e) {
+					// log exception...
+					System.out.println(e);
+				}
+			}
+		});
+	}
+	
+	private void start() {
+		new RadioCompetition();
+	}
+}
