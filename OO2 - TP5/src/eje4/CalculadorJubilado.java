7970ce63885f2cf53aa1f4d5aa754c1ab2df71cf
FeerGonzalez <fernando.g.gonzalez@hotmail.com>
2023-05-07 23:10:25 -0300

    detalles punto 4

--- OO2 - TP5/src/eje4/CalculadorJubilado.java
+++ OO2 - TP5/src/eje4/CalculadorJubilado.java
@@ -16,12 +16,4 @@ public class CalculadorJubilado extends Calculador {
 		return precioProducto + (precioProducto * 0.1);
 	}
 
-//	public double calcularPrecio(double precioProducto) {
-//		double precioTotal = precioProducto;
-//		if (!of(mesEnPromocion).equals(now().getMonth())) {
-//			precioTotal += precioProducto * 0.1;
-//		}
-//		log.log(CalculadorJubilado.class.getName());
-//		return precioTotal;
-//	}
 }
--- OO2 - TP5/src/eje4/CalculadorNoJubilado.java
+++ OO2 - TP5/src/eje4/CalculadorNoJubilado.java
@@ -16,14 +16,4 @@ public class CalculadorNoJubilado extends Calculador {
 		return precioProducto + (precioProducto * 0.21);
 	}
 
-//	public double calcularPrecio(double precioProducto) {
-//		double precioTotal = precioProducto;
-//		if (of(mesEnPromocion).equals(now().getMonth())) {
-//			precioTotal += precioProducto * 0.15;
-//		} else {
-//			precioTotal += precioProducto * 0.21;
-//		}
-//		log.log(CalculadorNoJubilado.class.getName());
-//		return precioTotal;
-//	}
 }
