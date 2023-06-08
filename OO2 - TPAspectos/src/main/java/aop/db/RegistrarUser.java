c3c8c3e06b886368691ea2c84263d0659b94e47d
FeerGonzalez <fernando.g.gonzalez@hotmail.com>
2023-06-08 12:28:01 -0300

    Tp de aspectos terminado ?

---
+++ OO2 - TPAspectos/src/main/java/aop/db/RegistrarUser.java
@@ -0,0 +1,35 @@
+package main.java.aop.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import main.java.aop.domain.Time;
+
+@Component
+public class RegistrarUser {
+	private String jdbcUrl = "jdbc:mysql://localhost:3306/tpaspectos-oo2"; // Buscar la forma de inyectar dependencia
+	private String username = "root";
+	private String password = "";
+
+	@Time
+	public void registrar(List<String> nombres) {
+		try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+				PreparedStatement statement = conn.prepareStatement("insert into usuario(nombre) values(?)",
+						PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+			for (String nombre : nombres) {
+				statement.setString(2, nombre);
+				statement.executeUpdate();
+			}
+
+			System.out.println("Personas agregadas correctamente a la base de datos.");
+		} catch (SQLException e) {
+			System.err.println("Error al agregar personas a la base de datos: " + e.getMessage());
+		}
+	}
+}
--- OO2 - TPAspectos/src/main/java/aop/web/UserController.java
+++ OO2 - TPAspectos/src/main/java/aop/web/UserController.java
@@ -1,36 +1,75 @@
 package main.java.aop.web;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import main.java.aop.domain.portsin.User;
-import main.java.aop.domain.portsin.Users;
+import main.java.aop.db.RegistrarUser;
 
 @RestController
 public class UserController {
 
-	private Users users;
+	private RegistrarUser registrar;
 
-	public UserController(Users users) {
-		this.users = users;
+	private static String[] names = { "Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Henry", "Isabella",
+			"Jack", "Javier", "Samuel", "Mateo", "Juan", "Jimmy", "Luis", "Osvaldo", "Alexis", "Reynaldo", "Gregorio" };
+
+//	private Users users;
+//
+//	public UserController(Users users) {
+//		this.users = users;
+//	}
+//
+//	@GetMapping("/users")
+//	public List<User> allUsers() {
+//		return this.users.users();
+//	}
+//
+//	@GetMapping("/users/{id}")
+//	public User usersById(@PathVariable int id) {
+//		return this.users.userById(id);
+//	}
+//
+//	@GetMapping("/users/search/{username}")
+//	public User usersByName(@PathVariable String username) {
+//		return this.users.usersByName(username);
+//	}
+
+	public UserController(RegistrarUser formaDeRegistro) {
+		this.registrar = formaDeRegistro;
 	}
 
-	@GetMapping("/users")
-	public List<User> allUsers() {
-		return this.users.users();
+	@GetMapping("/users/insertar/cinco")
+	public void insertarCincoUsuarios(@PathVariable int cantidad) {
+		insertarUsuarios(cantidad);
 	}
 
-	@GetMapping("/users/{id}")
-	public User usersById(@PathVariable int id) {
-		return this.users.userById(id);
+	@GetMapping("/users/insertar/cien")
+	public void insertarCienUsuarios(@PathVariable int cantidad) {
+		insertarUsuarios(cantidad);
 	}
 
-	@GetMapping("/users/search/{username}")
-	public User usersByName(@PathVariable String username) {
-		return this.users.usersByName(username);
+	private void insertarUsuarios(int cantidadUsuarios) {
+		registrar.registrar(generarListaPersonas(cantidadUsuarios));
+	}
+
+	private List<String> generarListaPersonas(int cantidad) {
+		int iterador = 0;
+		List<String> personList = new ArrayList<>();
+
+		while (iterador < cantidad) {
+			Random random = new Random();
+			String name = UserController.names[random.nextInt(names.length)];
+			personList.add(name);
+			iterador++;
+		}
+
+		return personList;
+
 	}
 
 }
